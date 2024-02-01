c5c964e0e2542cbce073bf8a121337ed379956b2
ohcu <158496774+ohcu@users.noreply.github.com>
2024-02-01 18:14:27 +0100

    Update script.js

--- apps/terminal/script.js
+++ apps/terminal/script.js
@@ -25,8 +25,8 @@ var commands = {
 };
 
 async function loadPlugins() {
-  plugins = ['https://sharptroubledentropy.wietsetd.repl.co/cheesgleapps/terminalplugins/commands.js','test.js','https://sharptroubledentropy.wietsetd.repl.co/cheesgleapps/terminalplugins/example.js','https://sharptroubledentropy.wietsetd.repl.co/cheesgleapps/terminalplugins/themetest.js']
-  //plugins = ['https://sharptroubledentropy.wietsetd.repl.co/cheesgleapps/terminalplugins/commands.js']
+  //plugins = ['https://sharptroubledentropy.wietsetd.repl.co/cheesgleapps/terminalplugins/commands.js','test.js','https://sharptroubledentropy.wietsetd.repl.co/cheesgleapps/terminalplugins/example.js','https://sharptroubledentropy.wietsetd.repl.co/cheesgleapps/terminalplugins/themetest.js']
+  plugins = ['https://ohcu.github.io/CheesgleApps/apps/terminal/plugins/commands.js']
   failed = []
   print(`loading ${plugins.length} plugin(s)...`,'lightgray')
   for (let i = 0; i < plugins.length; i++) {
