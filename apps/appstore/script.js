304573bd90aad249f263b7d55bdf39e1f8e27599
Wietse2647 <wietse2647@gmail.com>
2024-02-11 03:25:24 +0100

    Commit

--- apps/appstore/script.js
+++ apps/appstore/script.js
@@ -5,10 +5,19 @@ const appTemplate = `<div class="app">
   <p>Description</p>
   <span class="install">Install</span>
 </div>`
+const appTemplate2 = `<div class="app">
+  <img>
+  <h3>Title</h3>
+  <p class="category">Category</p>
+  <p>Description</p>
+  <span class="install">Install</span>
+</div>`
 
-let appsContainer = document.getElementById(`apps`);
-let main = document.getElementById(`main`);
+let browseContainer = document.getElementById(`browseApps`);
+let appsContainer = document.getElementById(`installedApps`);
 let loading = document.getElementById(`loading`);
+let main = document.getElementById(`apps`);
+let installed = document.getElementById(`installed`);
 
 function createElementFromHTML(htmlString) {
   var div = document.createElement('div');
@@ -20,7 +29,7 @@ async function loadApps() {
   await fetch(`https://ohcu.github.io/cheesgle-apps/apps/appstore/apps.json`).then(async (r) => {
     if (r.status == 200) {
       let json = await r.json()
-      appsContainer.innerHTML = ``
+      browseContainer.innerHTML = ``
       for (let i = 0; i < json.length; i++) {
         let html = createElementFromHTML(appTemplate)
         
@@ -53,21 +62,61 @@ async function loadApps() {
             addApp(appPath)
           }
         });
-        appsContainer.appendChild(html)
+        browseContainer.appendChild(html)
       }
     } else {
       loadApps()
     }
   }).catch(console.error)
-  loading.hidden = true
-  main.hidden = false
+}
+
+async function loadInstalled() {
+  appsContainer.innerHTML = ``
+  for (let i = 0; i < phoneApps.length; i++) {
+    let html = createElementFromHTML(appTemplate2)
+   
+    if (i.endsWith('/') { i.slice(0,-1); }
+    let appPath = i
+
+    let app
+    await fetch(appPath+`/package.json`).then(async (r) => {
+      console.log(r.json())
+      app = await r.json()
+    });
+
+    html.title = `${app.title}\nApp by ${app.author}\nPermissions: ${app.permissions.join(', ')}`
+    html.getElementsByTagName(`img`)[0].src = `${appPath}/${app.icon}`
+    html.getElementsByTagName(`h3`)[0].innerText = app.title
+    html.getElementsByTagName(`p`)[1].innerText = app.description
+    html.getElementsByTagName(`p`)[0].innerText = app.category
+    let install = html.getElementsByTagName(`span`)[0]
+    install.innerText = "Install"
+    if (phoneApps.includes(appPath)) {
+      install.innerText = "Uninstall"
+      install.classList = ["uninstall"]
+    }
+    install.addEventListener('click', () => {
+      if (phoneApps.includes(appPath)) {
+        install.innerText = "Install"
+        install.classList = ["install"]
+        removeApp(appPath)
+      }else{
+        install.innerText = "Uninstall"
+        install.classList = ["uninstall"]
+        addApp(appPath)
+      }
+    });
+    appsContainer.appendChild(html)
+  }
 }
 
 let phoneApps = []
 window.addEventListener('message', function ({ data }) {
   if (data.type == "info") {
     phoneApps = data.phoneApps;
-    loadApps()
+    await loadApps()
+    await loadInstalled()
+    goto('main')
   }
 });
 
@@ -124,4 +173,11 @@ function loadJSON(path, success, error) {
   };
   xhr.open('GET', path, true);
   xhr.send();
+}
+
+function goto(menu) {
+  loading.hidden = true
+  main.hidden = true
+  installed.hidden = true
+  menu.hidden = false
 }
